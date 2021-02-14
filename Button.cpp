99885ecc2e75a3c5213eddf223b93402d784696f
LIS <l.ilya@outlook.com>
2021-02-14 21:09:25 +0300

    Button class

---
+++ Button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by We on 14.02.21.
+//
+
+#include "Button.h"
---
+++ Button.h
@@ -0,0 +1,14 @@
+//
+// Created by We on 14.02.21.
+//
+
+#ifndef FOREST_BUTTON_H
+#define FOREST_BUTTON_H
+
+
+class Button {
+
+};
+
+
+#endif //FOREST_BUTTON_H

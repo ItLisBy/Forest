5a212fd32d8fcfc29b2ee66241b12b8d1098b757
LIS <l.ilya@outlook.com>
2021-02-14 23:22:35 +0300

    Upd

--- Button.cpp
+++ Button.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Button.h"
+
+Button::Button(const String &showed_text, const Vector2f &size, const int &char_size, const Color &bg_color, const Color &text_color) {
+    //Setting up showed text
+    _text.setString(showed_text);
+    _text.setFillColor(text_color);
+    _text.setCharacterSize(char_size);
+
+    //Setting up button view
+    _button.setSize(size);
+    _button.setFillColor(bg_color);
+}
+
+void Button::setFont(const Font &font) {
+    //Set font
+    _text.setFont(font);
+}
+
+void Button::changeBackColor(const Color &color) {
+    //Set button's background color
+    _button.setFillColor(color);
+}
--- Button.h
+++ Button.h
@@ -1,13 +1,42 @@
-//
-// Created by We on 14.02.21.
-//
-
 #ifndef FOREST_BUTTON_H
 #define FOREST_BUTTON_H
 
+#include "SFML/Graphics.hpp"
 
+using namespace sf;
+
+///
+/// \brief Button class
+///
 class Button {
+public:
+    ///
+    /// \param showed_text
+    /// Text that will be showed on button
+    /// \param size
+    /// Size of button like Vector2
+    /// \param char_size
+    /// Size of text
+    /// \param bg_color
+    /// Color of button background
+    /// \param text_color
+    /// Color of text on button
+    Button(const String &showed_text, const Vector2f &size, const int &char_size, const Color &bg_color, const Color &text_color);
 
+    /// \bried
+    /// Setting up font for text
+    /// \param font
+    /// Font that will be using
+    void setFont(const Font &font);
+
+    /// \brief
+    /// Change button's background color
+    /// \param color
+    /// Color for background
+    void changeBackColor(const Color &color);
+private:
+    RectangleShape _button;
+    Text _text;
 };
 
 

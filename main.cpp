8634220746a18a11a93234626f1c73b72aba4f4a
ŁIS <L.ilya@outlook.com>
2021-02-14 12:43:26 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+//Defines for window size
+#define WIDTH 1920
+#define HIGH 1080
+
+int main() {
+    Window window(VideoMode(800, 600), "Forest", Style::Titlebar | Style::Close);
+
+    window.setFramerateLimit(60);
+
+    //Loop program while it's window is open
+    while(window.isOpen()){
+        //Handling events
+        Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+
+    }
+
+    return 0;
+}

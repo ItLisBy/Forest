871456ec6ea4de32d1281a76773c7f6b7e17d041
LIS <l.ilya@outlook.com>
2021-04-16 20:57:43 +0300

    Add ImGui-SFML

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "SFML/Graphics.hpp"
-#include "Button.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
 
 using namespace sf;
 
@@ -10,56 +11,46 @@ using namespace sf;
 bool started = false;
 
 int main() {
+    bool stopped = true;
+
     RenderWindow window(VideoMode(WIDTH, HEIGHT), "Forest", Style::Titlebar | Style::Close);
+    window.setFramerateLimit(200);
+    ImGui::SFML::Init(window);
 
-    window.setFramerateLimit(60);
-
-    //Load main text font
-    Font main_font;
-    main_font.loadFromFile("fonts/LiberationSans-Regular.ttf");
-
-    //Load main texture
-    Texture main_texture;
-    main_texture.loadFromFile("textures/textures.png");
-
-    //Setup start/stop button
-    Button s_button("btn_s", main_texture,
-                    IntRect(0, 40, 160, 40),
-                    IntRect(0, 0, 160, 40),
-                    Vector2f(20.0f, 20.0f));
-
-        //Loop program while it's window is open
+    //Loop program while it's window is open
+    Clock delta_clock;
     while(window.isOpen()){
-        //Handling events
         Event event;
+        int process;
+        //Handling events
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(event);
+
             // "close requested" event: we close the window
             if (event.type == Event::Closed)
                 window.close();
-            else if (event.type == Event::MouseButtonPressed || event.type == Event::MouseButtonReleased)
-                if (s_button.isClicked(Mouse::getPosition(window))){
-                    //s_button.setText(IntRect(0, 80, 160, 40));
-                    s_button.s_btn_Click();
-                }
+
         }
 
+        ImGui::SFML::Update(window, delta_clock.restart());
+
+        if (stopped){
+            ImGui::Begin("Tools");
+            ImGui::
+            if(ImGui::Button("Start", ImVec2(160, 40))) {
+                stopped = false;
+            }
+            ImGui::End();
+
+        }
+
+
+
         window.clear(Color(255, 255, 255));
-
-        window.draw(s_button);
-
+        ImGui::SFML::Render(window);
         window.display();
     }
-
+    ImGui::SFML::Shutdown();
     return 0;
-}
-
-void Button::s_btn_Click() {
-    if (this->text_area_.top == 40) {
-        this->setText(IntRect(0, 80, 160, 40));
-    }
-    else if (this->text_area_.top == 80) {
-        this->setText(IntRect(0, 40, 160, 40));
-    }
-    started = !started;
 }
